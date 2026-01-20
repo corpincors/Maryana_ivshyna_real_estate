f13b65d57f722d360fc946b12e39304a5fb17972
Вадик K <vadikk@host-192-168-5-100.sohonet.ua>
2026-01-20 10:01:37 +0200

    Prepare for Vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Maryana_ivshyna_real_estate/',
+  base: '/',
   server: {
     port: 3000,
     host: '0.0.0.0',
